bfa5fcbcc6588465f0f2e4f0b7a0c58576be4777
minayekta <mina.qolizadeh@hotmail.com>
2024-05-27 23:13:56 -0700

    get req

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -19,7 +18,7 @@ function PostCard(postData: postData | any) {
           <h3 className="text-gray-800 pb-3">{postData.title}</h3>
           <p>{postData.description}</p>
         </div>
-        <Link href={`/blog/${postData.id}`} className="px-3 py-2 text-white bg-blue-700 mt-2">
+        <Link href={`/post/${postData.id}`} className="px-3 py-2 text-white bg-blue-700 mt-2">
           Details
         </Link>
       </div>
